2ab3ca7adda55cae61e39073ba0e6081215df5ac
hazrid93 <hazrid93@hotmail.com>
2019-01-25 20:03:26 +0800

    adding modal to add events

--- src/components/subcomponents/Events.vue
+++ src/components/subcomponents/Events.vue
@@ -3,8 +3,73 @@
         <v-container fluid>
             <!-- justify-content (horizontal), align-items (vertical) 
                 Note:cannot control xs6 sizes if use column layout -->
-            <v-layout justify-end align-start row wrap>
-                <v-flex xs8>
+            <v-layout justify-center row wrap>
+                <v-flex xs10 sm10 md4 :class="{'pb-4': paddingBottom, 'pr-4': paddingRight}">
+                    <v-layout row justify-center>
+                        <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+                        <v-btn class="ma-0" slot="activator" color="primary" dark>Open Dialog</v-btn>
+                        <v-card>
+                            <v-toolbar dark color="primary">
+                            <v-btn icon dark @click="dialog = false">
+                                <v-icon>close</v-icon>
+                            </v-btn>
+                            <v-toolbar-title>Settings</v-toolbar-title>
+                            <v-spacer></v-spacer>
+                            <v-toolbar-items>
+                                <v-btn dark flat @click="dialog = false">Save</v-btn>
+                            </v-toolbar-items>
+                            </v-toolbar>
+                            <v-list three-line subheader>
+                            <v-subheader>User Controls</v-subheader>
+                            <v-list-tile avatar>
+                                <v-list-tile-content>
+                                <v-list-tile-title>Content filtering</v-list-tile-title>
+                                <v-list-tile-sub-title>Set the content filtering level to restrict apps that can be downloaded</v-list-tile-sub-title>
+                                </v-list-tile-content>
+                            </v-list-tile>
+                            <v-list-tile avatar>
+                                <v-list-tile-content>
+                                <v-list-tile-title>Password</v-list-tile-title>
+                                <v-list-tile-sub-title>Require password for purchase or use password to restrict purchase</v-list-tile-sub-title>
+                                </v-list-tile-content>
+                            </v-list-tile>
+                            </v-list>
+                            <v-divider></v-divider>
+                            <v-list three-line subheader>
+                            <v-subheader>General</v-subheader>
+                            <v-list-tile avatar>
+                                <v-list-tile-action>
+                                <v-checkbox v-model="notifications"></v-checkbox>
+                                </v-list-tile-action>
+                                <v-list-tile-content>
+                                <v-list-tile-title>Notifications</v-list-tile-title>
+                                <v-list-tile-sub-title>Notify me about updates to apps or games that I downloaded</v-list-tile-sub-title>
+                                </v-list-tile-content>
+                            </v-list-tile>
+                            <v-list-tile avatar>
+                                <v-list-tile-action>
+                                <v-checkbox v-model="sound"></v-checkbox>
+                                </v-list-tile-action>
+                                <v-list-tile-content>
+                                <v-list-tile-title>Sound</v-list-tile-title>
+                                <v-list-tile-sub-title>Auto-update apps at any time. Data charges may apply</v-list-tile-sub-title>
+                                </v-list-tile-content>
+                            </v-list-tile>
+                            <v-list-tile avatar>
+                                <v-list-tile-action>
+                                <v-checkbox v-model="widgets"></v-checkbox>
+                                </v-list-tile-action>
+                                <v-list-tile-content>
+                                <v-list-tile-title>Auto-add widgets</v-list-tile-title>
+                                <v-list-tile-sub-title>Automatically add home screen widgets</v-list-tile-sub-title>
+                                </v-list-tile-content>
+                            </v-list-tile>
+                            </v-list>
+                        </v-card>
+                        </v-dialog>
+                    </v-layout>
+                </v-flex>
+                <v-flex xs10 sm10 md8>
                     <div v-if="events.length">
                         <template v-for="(event, index) in events">
                             <v-card :key="event.title" mb-3>
@@ -92,17 +157,37 @@
 
     export default {
         data() {
-            return { }
+            return { 
+                dialog: false,
+                notifications: false,
+                sound: true,
+                widgets: false,
+                paddingBottom: false,
+                paddingRight: false
+            }
                 
         },
         computed: {
             //use mapState to get access to all state properties or
             //use mapGetters to get all the getters specified in store.js
             //will use mapState for now
+ 
             ...mapState(['userProfile','currentUser','events'])
         },
         methods: {
-
+            onResize () {
+                console.log(this.$vuetify.breakpoint.name)
+                if(this.$vuetify.breakpoint.name=="xs" || this.$vuetify.breakpoint.name=="sm"){
+                    console.log("sm or xs")
+                    this.paddingBottom = true
+                    this.paddingRight = false
+                } else {
+                    console.log("md or larger")
+                    this.paddingBottom = false
+                    this.paddingRight = true
+                }
+                
+            }
         },
         filters: {
             formatDate(val) {
@@ -113,6 +198,15 @@
             trimLength(val) {
                 if (val.length < 200) { return val }
                 return `${val.substring(0, 200)}...`
+            }
+        },
+        mounted () {
+            this.onResize()
+            window.addEventListener('resize', this.onResize, { passive: true })
+        },
+        beforeDestroy () {
+            if (typeof window !== 'undefined') {
+            window.removeEventListener('resize', this.onResize, { passive: true })
             }
         }
     }
